7db3fe0a3e8cdc96f7bd178edaf76df079cfd174
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-02-11 11:06:01 +0200

    chore: Update for ts-jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,15 @@ module.exports = {
   verbose: true,
   testEnvironment: "node",
   coveragePathIgnorePatterns: ["/node_modules/"],
-  testPathIgnorePatterns: []
+  testPathIgnorePatterns: [],
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  globals: {
+    "ts-jest": {
+      tsConfigFile: "tsconfig.json"
+    }
+  },
+  testMatch: ["**/tests/*.+(ts|tsx|js)"]
 };
