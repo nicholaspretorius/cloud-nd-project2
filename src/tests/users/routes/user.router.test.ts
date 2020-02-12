4e86a8220da453803a9949e90e40b29494799b2e
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-02-12 11:42:30 +0200

    tests: Add POST /users

--- src/tests/users/routes/user.router.test.ts
+++ src/tests/users/routes/user.router.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import app from "./../../../app";
+import { User } from "./../../../users/models/User";
 
 describe("GET /users", () => {
     it("should return a status of 200", async () => {
@@ -12,5 +13,27 @@ describe("GET /users/:id", () => {
     it("should return a status of 200", async () => {
         const res = await request(app).get("/users/1");
         expect(res.status).toEqual(200);
+    });
+});
+
+describe("POST /users", () => {
+
+    afterEach(async () => {
+        User.destroy({
+            where: {},
+            truncate: true
+        });
+    });
+
+    it("should return a status of 200", async () => {
+
+        const data = {
+            "email": "test@test1.com",
+            "password": "password"
+        };
+
+        const res = await (await request(app).post("/users").send(data));
+        expect(res.status).toEqual(200);
+        expect(res.body.email).toEqual(data.email);
     });
 });
