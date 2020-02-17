c2a10951b9278a1363a8a42985a45bfb1fb15290
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-02-17 08:21:59 +0200

    tests: Add tests for GET /images/upload

--- src/tests/images/routes/image.router.test.ts
+++ src/tests/images/routes/image.router.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import app from "./../../../app";
+import { User } from "./../../../users/models/User";
 
 describe("GET /images/filtered", () => {
 
@@ -33,5 +34,47 @@ describe("GET /images/filtered", () => {
     it("should return a status of 200 if a valid image_url is provided", async () => {
         const res = await request(app).get(`/images/filtered?image_url=${image_url}`);
         expect(res.status).toEqual(200);
+    });
+});
+
+describe("GET /images/upload", () => {
+
+    afterEach(async () => {
+        User.destroy({
+            where: {},
+            truncate: true
+        });
+    });
+
+    it("should return a status of 401 if not authorization header is provided", async () => {
+        const res = await request(app).get("/images/upload");
+        expect(res.status).toEqual(401);
+        expect(res.body.message).toBe("No authorization headers");
+    });
+
+    it("should return a status of 401 if authorization header is invalid", async () => {
+        const res = await request(app).get("/images/upload").set('Authorization', 'invalid');
+        expect(res.status).toEqual(401);
+        expect(res.body.message).toBe("Invalid token");
+    });
+
+    it("should return a status of 401 if token is invalid", async () => {
+        const res = await request(app).get("/images/upload").set('Authorization', 'Bearer invalid');
+        expect(res.status).toEqual(500);
+        expect(res.body.message).toBe("Invalid token");
+    });
+
+    it("should return a status of 200 if token is valid", async () => {
+        const userData = {
+            "email": "test@test.com",
+            "password": "123456"
+        }
+        const res1 = await request(app).post("/auth/register").send(userData);
+        expect(res1.status).toEqual(201);
+        const token = res1.body.token;
+
+        const res = await request(app).get("/images/upload").set('Authorization', `Bearer ${token}`);
+        expect(res.status).toEqual(200);
+        expect(res.body.message).toBe("Authorized");
     });
 });
