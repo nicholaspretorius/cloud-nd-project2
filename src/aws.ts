f2bf5f194e89c2cbb7a0b715f660fe6b4e91659e
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-02-18 08:13:26 +0200

    feat: Cleanup, remove Image model

--- src/aws.ts
+++ src/aws.ts
@@ -14,12 +14,7 @@ export const s3 = new AWS.S3({
     params: { Bucket: c.aws_media_bucket }
 });
 
-/* getGetSignedUrl generates an aws signed url to retreive an item
- * @Params
- *    key: string - the filename to be put into the s3 bucket
- * @Returns:
- *    a url as a string
- */
+// getGetSignedUrl generates an aws signed url to retreive an item
 export function getGetSignedUrl(key: string): string {
 
     const signedUrlExpireSeconds = 60 * 5
@@ -33,12 +28,7 @@ export function getGetSignedUrl(key: string): string {
     return url;
 }
 
-/* getPutSignedUrl generates an aws signed url to put an item
- * @Params
- *    key: string - the filename to be retreived from s3 bucket
- * @Returns:
- *    a url as a string
- */
+// getPutSignedUrl generates an aws signed url to put an item
 export function getPutSignedUrl(key: string) {
 
     const signedUrlExpireSeconds = 60 * 5
